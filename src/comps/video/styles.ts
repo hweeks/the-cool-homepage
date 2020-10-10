6f820025e5027a999f7740826522f48f44841468
Harris Weeks <root@hweeks.com>
2020-10-10 13:48:25 -0700

    feat: fix ios not resuming video

---
+++ src/comps/video/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HomeVideo = styled.video`
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 0;
+`;
---
+++ src/conts/content/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from '../../comps/header';
+import {
+  ActualContent, MainText,
+} from './styles';
+import { DimensionalText } from '../../comps/text';
+import { Footer } from '../../comps/footer';
+
+export const Content = () => (
+  <ActualContent>
+    <Header />
+    <MainText>
+      <DimensionalText text="i like bike" />
+    </MainText>
+    <Footer />
+  </ActualContent>
+);
